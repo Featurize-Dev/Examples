6809a0e55ae63414724a2baa22655ecb80b56bc1
Patrick Evers <p.evers@tjip.com>
2023-11-27 16:22:32 +0100

    Changes

--- eShop/src/Ordering/Features/Order/Entities/OrderStatus.cs
+++ eShop/src/Ordering/Features/Order/Entities/OrderStatus.cs
@@ -1,5 +1,30 @@
-﻿namespace Ordering.Features.Order.Entities;
+﻿using System.Linq;
 
-public class OrderStatus
+namespace Ordering.Features.Order.Entities;
+
+public class OrderStatus(int id, string name)
 {
+
+    public static OrderStatus Submitted = new OrderStatus(1, nameof(Submitted).ToLowerInvariant());
+
+    public int Id { get; } = id;
+    public string Name { get; } = name;
+
+    public static IEnumerable<OrderStatus> List() =>
+        new[] { Submitted };
+
+    public static OrderStatus FromName(string name)
+    {
+        var state = List()
+            .SingleOrDefault(s => string.Equals(s.Name, name, StringComparison.CurrentCultureIgnoreCase));
+
+        return state ?? throw new InvalidOperationException();
+    }
+
+    public static OrderStatus FromId(int id)
+    {
+        var state = List() 
+            .SingleOrDefault(s => s.Id == id);
+        return state ?? throw new InvalidOperationException();
+    }
 }
