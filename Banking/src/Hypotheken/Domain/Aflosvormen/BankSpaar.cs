3bd2aea58517a0053f195cb8db8abbb2cb2359c0
Patrick Evers <pmdevers@gmail.com>
2024-06-28 16:05:57 +0200

    Auto stash before merge of "GoldCreditBank" and "origin/GoldCreditBank"

--- Banking/src/Hypotheken/Domain/Aflosvormen/BankSpaar.cs
+++ Banking/src/Hypotheken/Domain/Aflosvormen/BankSpaar.cs
@@ -1,13 +1,21 @@
-﻿using Common.ValueObjects;
+using Common.ValueObjects;
 
 namespace Hypotheken.Domain.Aflosvormen;
-public sealed class BankSpaar : Aflosvorm
+public sealed class BankSpaar(Percentage spaarRente) : Aflosvorm
 {
     public Percentage Boetevrij
         => Percentage.Create(10);
 
+    public Percentage SpaarRente { get; } = spaarRente;
+
     public Amount GetAflossing(Leningdeel leningdeel, Amount rente, int termijn)
     {
-        return 0;
+        var hoofdsom = (double)leningdeel.Hoofdsom;
+        var maandSpaarRente = (double)SpaarRente / 12;
+        double factor = Math.Pow(1 + (double)maandSpaarRente, termijn);
+        
+        double maandelijkseInleg = (hoofdsom * maandSpaarRente) / (factor - 1);
+
+        return Amount.Create(maandelijkseInleg, leningdeel.Hoofdsom.Currency);
     }
 }
